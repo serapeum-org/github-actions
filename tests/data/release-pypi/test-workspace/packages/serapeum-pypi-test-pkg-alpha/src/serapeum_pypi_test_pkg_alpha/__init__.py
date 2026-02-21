65410006649b5ac86a865e6ec04e153dcee8fe4a
Mostafa Farrag <moah.farag@gmail.com>
2026-02-21 20:58:08 +0100

    add new test data

---
+++ tests/data/release-pypi/test-workspace/packages/serapeum-pypi-test-pkg-alpha/src/serapeum_pypi_test_pkg_alpha/__init__.py
@@ -0,0 +1,3 @@
+"""Alpha sub-package for PyPI release action workspace tests."""
+
+__version__ = "0.1.0"
---
+++ tests/data/release-pypi/test-workspace/packages/serapeum-pypi-test-pkg-beta/src/serapeum_pypi_test_pkg_beta/__init__.py
@@ -0,0 +1,3 @@
+"""Beta sub-package for PyPI release action workspace tests."""
+
+__version__ = "0.1.0"
